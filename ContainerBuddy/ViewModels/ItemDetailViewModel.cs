58a10ad8c850f5d0195f883bd42720f8bc3d6838
Alfred Neumayer <dev.beidl@gmail.com>
2020-12-15 02:25:44 +0100

    Initial commit

---
+++ ContainerBuddy/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ContainerBuddy.Models;
+
+namespace ContainerBuddy.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
