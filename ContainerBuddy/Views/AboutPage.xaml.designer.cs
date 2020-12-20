1c01cb01b4c40d25cfd76a8df1408fd13f95c227
Alfred E. Neumayer <dev.beidl@gmail.com>
2020-12-20 04:06:29 +0100

    Views: Add designer.cs files
    Files generated by Continuous

---
+++ ContainerBuddy/Views/AboutPage.xaml.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <
+//     
+//     4.0.50524.0
+//
+//     
+//     
+// </
+//------------------------------------------------------------------------------
+
+[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("", "", typeof(global::ContainerBuddy.Views.AboutPage))]
+
+namespace ContainerBuddy.Views {
+    
+    
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/private/var/mobile/Containers/Shared/AppGroup/674FE21B-A808-4CAE-9714-FC3FD4AEA4" +
+    "0C/File Provider Storage/Repositories/ContainerBuddy/ContainerBuddy/Views/AboutP" +
+    "age.xaml")]
+    public partial class AboutPage : global::Xamarin.Forms.ContentPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private void InitializeComponent() {
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(AboutPage));
+        }
+    }
+}
---
+++ ContainerBuddy/Views/NewItemPage.xaml.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <
+//     
+//     4.0.50524.0
+//
+//     
+//     
+// </
+//------------------------------------------------------------------------------
+
+[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("", "", typeof(global::ContainerBuddy.Views.NewItemPage))]
+
+namespace ContainerBuddy.Views {
+    
+    
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/private/var/mobile/Containers/Shared/AppGroup/674FE21B-A808-4CAE-9714-FC3FD4AEA4" +
+    "0C/File Provider Storage/Repositories/ContainerBuddy/ContainerBuddy/Views/NewIte" +
+    "mPage.xaml")]
+    public partial class NewItemPage : global::Xamarin.Forms.ContentPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private void InitializeComponent() {
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(NewItemPage));
+        }
+    }
+}
